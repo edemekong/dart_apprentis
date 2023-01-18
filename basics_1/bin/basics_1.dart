e9785370ff7f5e79569d58414f484bead19cfb7c
Paul Edemekong <hello.paulvin@gmail.com>
2023-01-18 15:22:17 +0100

    leave an assignment

--- basics_1/bin/basics_1.dart
+++ basics_1/bin/basics_1.dart
@@ -1,1 +1,17 @@
-void main() {}
+void main() {
+  workingArea();
+}
+
+//  Create a constant named myAge and initialize it with your age.
+//  Write an if statement to print out “Teenager” if your age is between 13 and 19,
+//  and “Not a teenager” if your age is not between 13 and 19.
+
+void workingArea() {
+  // CODE START
+
+  int x = 10;
+
+  print(x > 10 ? "Yes!" : "No!");
+
+  // CODE END
+}
