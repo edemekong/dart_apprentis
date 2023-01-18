2721b0bb1128139630239f7769575fdc6547f710
Paul Edemekong <hello.paulvin@gmail.com>
2023-01-18 14:48:23 +0100

    remove unsed files

--- basics_1/bin/basics_1.dart
+++ basics_1/bin/basics_1.dart
@@ -1,5 +1,1 @@
-import 'package:basics_1/basics_1.dart' as basics_1;
-
-void main(List<String> arguments) {
-  print('Hello world: ${basics_1.calculate()}!');
-}
+void main() {}
--- basics_1/test/basics_1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:basics_1/basics_1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
